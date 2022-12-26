54d7404007600287264cee84cadb789be0870a91
Abubakir Kais <bakir_janabi@yahoo.com>
2022-12-26 18:12:47 +0400

    Adding grpc server side code

--- main.go
+++ main.go
@@ -1,5 +1,52 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/Bruary/transactions-service/server/pb"
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+type TransactionsServer struct {
+	pb.UnimplementedTransactionsServer
+}
+
+func (s *TransactionsServer) CreateTransaction(ctx context.Context, req *pb.CreateTransactionRequest) (*pb.CreateTransactionResponse, error) {
+	fmt.Println("The request: %v", req.GetAmount(), req.GetCurrency())
+	user_uid := uuid.New()
+
+	return &pb.CreateTransactionResponse{
+		Uid:      user_uid.String(),
+		Amount:   req.GetAmount(),
+		Currency: req.GetCurrency(),
+	}, nil
+}
+
 func main() {
+
+	// open connection
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatal("Failed to listen: %v", err)
+	}
+	// Create a new grpc server
+	s := grpc.NewServer()
+
+	// register the transactions server
+	pb.RegisterTransactionsServer(s, &TransactionsServer{})
+	log.Printf("Server listening at %v ", lis.Addr())
+
+	// start reading and parsing the input
+	if err = s.Serve(lis); err != nil {
+		log.Fatal("Failed to serve: %v", err)
+	}
 
 }
